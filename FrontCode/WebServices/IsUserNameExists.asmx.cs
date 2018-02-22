23bd7cd63f5c0479796594f7aa5f37a3750e5465
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-22 14:49:06 +0600

    Adding web service for distinct username and email

---
+++ FrontCode/WebServices/IsUserNameExists.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using BusinessAccessLayer;
+using System.Data;
+using System.Web.Script.Serialization;
+
+namespace FrontCode.WebServices
+{
+    /// <summary>
+    /// IsUserNameExists going to provide us the information
+    /// whether an username was already exists or not
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class IsUserNameExists : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public void UserNameExists(string username)
+        {
+            User user = new User
+            {
+                Username = username
+            };
+
+            user.UserNameInUse = false;
+
+            FrontCodeBusiness fcb = new FrontCodeBusiness
+            {
+                UserObj = user
+            };
+
+            DataTable dt = fcb.IsUserNameExists();
+
+            if (dt.Rows.Count > 0)
+            {
+                user.UserNameInUse = dt.Rows[0]["UserNameExists"].ToString().Equals("0") ? false : true;
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(user));
+        }
+    }
+}
