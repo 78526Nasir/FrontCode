41311a6504597a57651d570a4693842c30a52c4c
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-24 00:54:33 +0600

    Admin Class Created

---
+++ BusinessAccessLayer/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class Admin
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string ProfileImage { get; set; }
+        public string Password { get; set; }
+
+    }
+}
