277a4c35b07a468fc9ec1c9290c315e4d310b535
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-24 00:56:41 +0600

    Syllabus UI created

--- FrontCode/SyllabusUI/HTMLCourse.aspx.cs
+++ FrontCode/SyllabusUI/HTMLCourse.aspx.cs
@@ -5,13 +5,19 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace FrontCode.CourseMaterial
+namespace FrontCode.SyllabusUI
 {
     public partial class HTMLCourse : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            Session["AbsolutePage"] = HttpContext.Current.Request.Url.AbsolutePath;
+        }
+
+        protected void btnEnrollHTML_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/CourseMaterial/HTML.aspx");
         }
     }
 }
