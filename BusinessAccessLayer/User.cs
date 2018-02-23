90b37fa8b2c22daf24f3435090519d9252f76890
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-24 00:54:49 +0600

    Adding New Property

--- BusinessAccessLayer/User.cs
+++ BusinessAccessLayer/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +9,48 @@ namespace BusinessAccessLayer
 {
     public class User
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Fullname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Salt { get; set; }
         public string Hash { get; set; }
+        public string ProfileImage { get; set; }
         public bool EmailInUse { get; set; }
         public bool UserNameInUse { get; set; }
-        public string ProfileImage { get; set; }
         public string GlobalUniqueIDForResetPassword { get; set; }
 
         // Properties are initialize on the fly
+
+
+
+        
+        public DataTable Login()
+        {
+            FrontCodeBusiness fcb = new FrontCodeBusiness
+            {
+                UserObj = this
+            };
+
+            return fcb.SelectUser();
+        }
+
+        public bool Registration()
+        {
+            FrontCodeBusiness fcb = new FrontCodeBusiness
+            {
+                UserObj = this
+            };
+
+            fcb.AddNewUser();
+
+            return true;
+        }
+
+        public void SetHashAndSalt(string hash, string salt)
+        {
+            this.Hash = hash;
+            this.Salt = salt;
+        }
     }
 }
