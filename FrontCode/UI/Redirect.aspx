c69b1c8d0547217a67ab99147219a81a145444d5
78526Nasir <islam35-1102@diu.edu.bd>
2018-02-24 00:56:55 +0600

    Redirect UI

---
+++ FrontCode/UI/Redirect.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Redirect.aspx.cs" Inherits="FrontCode.UI.Redirect" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Redirect</title>
+    <style>
+        @font-face {
+            font-family: 'OpenSans-Light';
+            src: url('../fonts/OpenSans-Light.ttf') format('truetype');
+        }
+
+        .message {
+            width:100%;
+            margin-top: 150px;
+            color: #40aadd;
+            font-size: 40px;
+            font-family: 'OpenSans-Light', Calibri;
+            font-weight:normal !important;
+            text-align:center;
+        }
+        a{
+            color:#ff8888;
+            text-decoration:none;
+        }
+        a:hover{
+            text-decoration:underline;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container message">
+            <span>Account Successfully Created. Redirect To <a href="UserLogin.aspx">Login</a> Page.</span>
+        </div>
+    </form>
+</body>
+</html>
--- FrontCode/UI/Registration.aspx.cs
+++ FrontCode/UI/Registration.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessAccessLayer;
+using FrontCode.Libraries;
+using System.Data;
 
 namespace FrontCode.UI
 {
@@ -12,6 +15,96 @@ namespace FrontCode.UI
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            if (Page.IsValid)
+            {
+                string imagePath = DefaultImagePath();
+
+                User user = new User
+                {
+                    UserName = txtUsername.Text,
+                    Fullname = txtFullName.Text,
+                    Password = txtPassword.Text,
+                    Email = txtEmail.Text,
+                    ProfileImage = imagePath
+                };
+
+                string salt;
+                string hash = HashingAndSalting.CreateSaltedHash(user.Password, out salt);
+
+                user.SetHashAndSalt(hash, salt);
+
+                if (!IsAlreadyExists())
+                {
+                    user.Registration();
+                    ResetForm();
+
+                    Response.Redirect("~/UI/Redirect.aspx");
+                }
+                else
+                {
+                    ResetForm();
+                    lblStatus.Text = "Username or Email already exists!";
+                }
+            }
+            else
+            {
+                ResetForm();
+                ValidationSummary1.HeaderText = "Please fill up all the necessary fields";
+            }
+        }
+
+        private bool IsAlreadyExists()
+        {
+            bool uExists = false;
+            bool eExists = false;
+
+            User user = new User
+            {
+                UserName = txtUsername.Text,
+                Email = txtEmail.Text
+            };
+
+            FrontCodeBusiness fcb = new FrontCodeBusiness
+            {
+                UserObj = user
+            };
+
+            DataTable dt = fcb.IsUserNameExists();
+
+            if (dt.Rows.Count > 0)
+            {
+                uExists = dt.Rows[0]["UserNameExists"].ToString().Equals("0") ? false : true;
+            }
+
+            dt = fcb.IsEmailExists();
+
+            if (dt.Rows.Count > 0)
+            {
+                eExists = dt.Rows[0]["EmailExists"].ToString().Equals("0") ? false : true;
+            }
+
+            return (uExists == false && eExists == false) ? false : true;
+
+        }
+
+        private string DefaultImagePath()
+        {
+            return "../images/avatar.png";
+        }
+
+        private void ResetForm()
+        {
+            txtUsername.Text = string.Empty;
+            txtFullName.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtConfirmPassword.Text = string.Empty;
+            cbAgreement.Checked = false;
         }
     }
 }
