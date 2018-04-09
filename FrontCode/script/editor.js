16f09969f13c0b734ffc64f757ed6cacd0d1707d
78526Nasir <islam35-1102@diu.edu.bd>
2018-04-10 03:32:40 +0600

    Editor js added

---
+++ FrontCode/script/editor.js
@@ -0,0 +1,44 @@
+﻿function update() {
+    var idoc = document.getElementById('iframe').contentWindow.document;
+
+    idoc.open();
+    idoc.write(editor.getValue());
+    idoc.close();
+}
+
+function setupEditor() {
+    window.editor = ace.edit("editor");
+    editor.setTheme("ace/theme/clouds_midnight"); // clouds_midnight, monokio_industrial
+    editor.getSession().setMode("ace/mode/html");
+    editor.setValue(`<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+</body>
+</html>`, 1); //1 = moves cursor to end
+
+    editor.getSession().on('change', function () {
+        update();
+    });
+
+    editor.focus();
+    editor.setOptions({
+        fontFamily: "consolas",
+        fontSize: "11pt",
+        showLineNumbers: true,
+        showGutter: true,
+        vScrollBarAlwaysVisible: true,
+        enableBasicAutocompletion: true, enableLiveAutocompletion: true,
+
+    });
+
+    editor.setShowPrintMargin(false);
+    editor.setBehavioursEnabled(false);
+}
+
+function ready() {
+    setupEditor();
+    update();
+}
