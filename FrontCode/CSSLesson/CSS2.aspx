ba8038b52288281c4e42d9405393527f2c9548cb
78526Nasir <islam35-1102@diu.edu.bd>
2018-04-14 09:38:05 +0600

    Second Topic in CSS

---
+++ FrontCode/CSSLesson/CSS2.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Title="CSS Lesson | 2" Language="C#" MasterPageFile="~/UI/Index.Master" AutoEventWireup="true" CodeBehind="CSS2.aspx.cs" Inherits="FrontCode.CSSLesson.CSS2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="body" runat="server">
+    <div class="d-head">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 left-sidebar">
+                    <ul>
+                        <li><a href="../CourseMaterial/CourseCSS.aspx">CSS কি?</a></li>
+                        <li><a class="active" href="#">CSS আমাদের কেনো দরকার</a></li>
+                        <li><a href="CSS3.aspx">CSS এর ফরমেট</a></li>
+                        <li><a href="#">CSS কিভাবে ব্যাবহার করতে হয়</a></li>
+                        <li><a href="#">CSS এর Property এবং Value সমূহ</a></li>
+                        <li>
+                            <a href="#">
+                                <code class="code">&lt;background&gt;</code>
+                                <code class="code">&lt;color&gt;</code>
+                                <code class="code">&lt;border&gt;</code>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <code class="code">&lt;height&gt;</code>
+                                <code class="code">&lt;width&gt;</code>
+                                <code class="code">&lt;outline&gt;</code>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <code class="code">&lt;margin&gt;</code>
+                                <code class="code">&lt;padding&gt;</code>
+                            </a>
+                        </li>
+
+                        <li><a href="#">CSS এর Selector সমূহ</a></li>
+                        <li><a href="#">CSS এ Element Selector সমূহ</a></li>
+                        <li><a href="#">CSS এ ID Selector সমূহ</a></li>
+                        <li><a href="#">CSS এ Class Selector সমূহ</a></li>
+                        <li><a href="#">CSS এ Group Selector সমূহ</a></li>
+                        <li><a href="#">CSS এ ফন্টসমূহ</a></li>
+                        <li><a href="#">CSS এর উপর কুইজ</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-8 right-sidebar" style="min-height: 560px;">
+                    <img src="../images/why.png" class="img-responsive img-style" />
+                    <br />
+                    <h1 class="title">CSS আমাদের কেনো দরকার</h1>
+                    <p>
+                        <code>CSS</code> একটি ওয়েবপেজের <b>স্টাইল</b>, <b>লেআউট</b>, <b>ধরণ</b>, <b>ডিভাইস কম্পিটাবিলিটি</b> ডিফাইন করে । <code>CSS</code> অনেক লাইটওয়েট, এটা লোড হতে সময় একেবারেই কম নেয় ।
+                        তাই ডেভেলপারদের কাছে <code>CSS</code> খুবি গুরুত্বপূর্ণ । যেকোনো ওয়েবসাইটের <code>CSS</code> এরবারি লোড হয়ে সেটা <code>Cache</code> মেমোরিতে ষ্টোর হয়ে যায়, যার
+                        ফলে পেজ লোড হতে সময় কম নেয় ।
+
+                        শুধু <code>HTML</code> দিয়ে একটা সুন্দর ওয়েবসাইট আশা করা অসম্ভব । <code>HTML</code> কে জীবন্ত করে হলো <code>CSS</code> । <code>CSS</code> সব ব্রাউজার সাপোর্ট করে ।
+                        <code>CSS</code> স্টাইল একটা ওয়েবসাইটকে যেভাবে খুশি সেই ভাবে ডিজাইন করার সক্ষমতা প্রদান করে । 
+                    </p>
+                    <br />
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
--- FrontCode/HTMLLesson/HTMLFinal.aspx.cs
+++ FrontCode/HTMLLesson/HTMLFinal.aspx.cs
@@ -11,7 +11,223 @@ namespace FrontCode.HTMLLesson
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            MultiView1.ActiveViewIndex = 0;
+            if (!IsPostBack)
+            {
+                MultiView1.ActiveViewIndex = 0;
+                btnPrev.Enabled = false;
+                btnPrev.Style.Add("cursor", "not-allowed");
+                //ScoreDiv.Style.Add("display", "none");
+            }
+        }
+
+        protected void btnPrev_Click(object sender, EventArgs e)
+        {
+            if(MultiView1.ActiveViewIndex == 0)
+            {
+                MultiView1.ActiveViewIndex = 0;
+                btnPrev.Enabled = false;
+                btnPrev.Style.Add("cursor", "not-allowed");
+            }
+            else if(MultiView1.ActiveViewIndex == 1)
+            {
+                btnPrev.Enabled = true;
+                btnPrev.Style.Add("cursor", "pointer");
+                MultiView1.ActiveViewIndex = 0;
+            }else if(MultiView1.ActiveViewIndex == 2)
+            {
+                MultiView1.ActiveViewIndex = 1;
+            }
+            else if (MultiView1.ActiveViewIndex == 3)
+            {
+                MultiView1.ActiveViewIndex = 2;
+            }
+            else if (MultiView1.ActiveViewIndex == 4)
+            {
+                MultiView1.ActiveViewIndex = 3;
+            }
+            else if (MultiView1.ActiveViewIndex == 5)
+            {
+                MultiView1.ActiveViewIndex = 4;
+            }
+            else if (MultiView1.ActiveViewIndex == 6)
+            {
+                MultiView1.ActiveViewIndex = 5;
+            }
+            else if (MultiView1.ActiveViewIndex == 7)
+            {
+                MultiView1.ActiveViewIndex = 6;
+            }
+            else if (MultiView1.ActiveViewIndex == 8)
+            {
+                MultiView1.ActiveViewIndex = 7;
+            }
+            else if (MultiView1.ActiveViewIndex == 9)
+            {
+                MultiView1.ActiveViewIndex = 8;
+            }
+
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            if (MultiView1.ActiveViewIndex == 0)
+            {
+                MultiView1.ActiveViewIndex = 1;
+            }
+            else if (MultiView1.ActiveViewIndex == 1)
+            {
+                MultiView1.ActiveViewIndex = 2;
+            }
+            else if (MultiView1.ActiveViewIndex == 2)
+            {
+                MultiView1.ActiveViewIndex = 3;
+            }
+            else if (MultiView1.ActiveViewIndex == 3)
+            {
+                MultiView1.ActiveViewIndex = 4;
+            }
+            else if (MultiView1.ActiveViewIndex == 4)
+            {
+                MultiView1.ActiveViewIndex = 5;
+            }
+            else if (MultiView1.ActiveViewIndex == 5)
+            {
+                MultiView1.ActiveViewIndex = 6;
+            }
+            else if (MultiView1.ActiveViewIndex == 6)
+            {
+                MultiView1.ActiveViewIndex = 7;
+            }
+            else if (MultiView1.ActiveViewIndex == 7)
+            {
+                MultiView1.ActiveViewIndex = 8;
+            }
+            else if (MultiView1.ActiveViewIndex == 8)
+            {
+                MultiView1.ActiveViewIndex = 9;
+            }else
+            {
+                MultiView1.ActiveViewIndex = 10;
+                CalculateScore();
+            }
+            
+        }
+
+        private void CalculateScore()
+        {
+            int score = 0;
+
+            if (Convert.ToInt32(Session["Q1"]) == 2)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q2"]) == 1)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q3"]) == 2)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q4"]) == 3)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q5"]) == 1)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q6"]) == 2)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q7"]) == 3)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q8"]) == 2)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q9"]) == 1)
+            {
+                score++;
+            }
+            if (Convert.ToInt32(Session["Q10"]) == 3)
+            {
+                score++;
+            }
+
+
+            //if (score == 0)
+            //{
+            //    btnViewDetails.Style.Add("display", "none");
+            //}else
+            //{
+            //    btnViewDetails.Style.Add("display", "block");
+            //}
+
+            lblScore.Text = score.ToString();
+        }
+
+        protected void rbl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q1"] = rbl1.SelectedIndex;
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q2"] = RadioButtonList1.SelectedIndex;
+        }
+
+        protected void RadioButtonList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q3"] = RadioButtonList2.SelectedIndex;
+        }
+
+        protected void RadioButtonList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q4"] = RadioButtonList3.SelectedIndex;
+        }
+
+        protected void RadioButtonList4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q5"] = RadioButtonList4.SelectedIndex;
+        }
+
+        protected void RadioButtonList5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q6"] = RadioButtonList5.SelectedIndex;
+        }
+
+        protected void RadioButtonList6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q7"] = RadioButtonList6.SelectedIndex;
+        }
+
+        protected void RadioButtonList7_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q8"] = RadioButtonList7.SelectedIndex;
+        }
+
+        protected void RadioButtonList8_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q9"] = RadioButtonList8.SelectedIndex;
+        }
+
+        protected void RadioButtonList9_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Session["Q10"] = RadioButtonList9.SelectedIndex;
+        }
+
+        protected void btnViewDetails_Click(object sender, EventArgs e)
+        {
+            ScoreDiv.Style.Add("display", "block");
+        }
+
+        protected void btnFinish_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/CourseMaterial/CourseHTML.aspx");
         }
     }
 }
